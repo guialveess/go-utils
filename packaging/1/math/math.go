9962e96b1769825dabda55dea645d7fc2154a270
guialveess <97guilherme.alves@gmail.com>
2026-01-10 23:10:14 -0300

    add packaging 1 math package

---
+++ packaging/1/math/math.go
@@ -0,0 +1,30 @@
+package math
+
+import "fmt"
+
+var Hello string = "Hello, Math!"
+
+type Math struct {
+	A int
+	B int
+}
+
+var result int
+
+func (m *Math) Add() int {
+	result = m.A + m.B
+	fmt.Println("A soma é:", result)
+	return result
+}
+
+func (m *Math) Subtract() int {
+	return m.A - m.B
+}
+
+func (m *Math) Multiply() int {
+	return m.A * m.B
+}
+
+func (m *Math) Divide() int {
+	return m.A / m.B
+}
